ba5cf440b29364e4fd8e83e4a633f102bc4af5ff
Régis Hanol <regis@hanol.fr>
2024-08-14 16:02:48 +0200

    Update config.nix

--- config.nix
+++ config.nix
@@ -1,5 +1,8 @@
 {
-  # Use the "systemd-boot" boot manager
+  # Update CPU's microcode
+  hardware.cpu.intel.updateMicrocode = true;
+
+  # Use the "systemd-boot" boot loader
   boot.loader.systemd-boot.enable = true;
 
   # NixOS version this flake was initially created on
