e7dfe026fdb478022b6862782a77498aedba2a12
Régis Hanol <regis@hanol.fr>
2024-08-14 12:14:47 +0200

    Create disko.nix

---
+++ disko.nix
@@ -0,0 +1,47 @@
+{ disko, ... }: {
+  imports = [ disko.nixosModules.disko ];
+
+  disko.devices = {
+    disk.main = {
+      type = "disk";
+      device = "/dev/nvme0n1";
+      content = {
+        type = "gpt";
+        partitions = {
+          ESP = {
+            type = "EF00";
+            size = "512M";
+            content = {
+              type = "filesystem";
+              format = "vfat";
+              mountpoint = "/boot";
+            };
+          };
+          luks = {
+            size = "100%";
+            content = {
+              type = "luks";
+              name = "luks";
+              settings.allowDiscards = true;
+              content = {
+                type = "filesystem";
+                format = "ext4";
+                mountpoint = "/nix";
+              };
+            };
+          };
+        };
+      };
+    };
+    nodev = {
+      "/" = {
+        fsType = "tmpfs";
+        mountOptions = [ "size=128M" "defaults" "mode=755" ];
+      };
+      "/home" = {
+        fsType = "tmpfs";
+        mountOptions = [ "size=128M" "defaults" "mode=0755" ];
+      };
+    };
+  };
+}
