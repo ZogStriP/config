ca2803ad5d8611bc64432bf434013852372344c3
Régis Hanol <regis@hanol.fr>
2024-08-14 18:07:26 +0200

    Update config.nix

--- config.nix
+++ config.nix
@@ -5,6 +5,9 @@
   # Use the "systemd-boot" boot loader
   boot.loader.systemd-boot.enable = true;
 
+  # System's timezone
+  time.timeZone = "Europe/Paris";
+
   # NixOS version this flake was initially created on
   system.stateVersion = "24.05";
 }
