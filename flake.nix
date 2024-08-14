397607ecd24d4c517191a7ccd21bcda6a5c8dfea
Régis Hanol <regis@hanol.fr>
2024-08-14 12:01:34 +0200

    Create flake.nix

---
+++ flake.nix
@@ -0,0 +1,12 @@
+{
+  description = "ZogStriP's NixOS flake."
+
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+
+  outputs = { self, nixpkgs }: {
+    nixosConfigurations.framezork = nixpkgs.lib.nixosSystem {
+      system = "x86_64-linux";
+      boot.loader.systemd-boot.enable = true;
+    };
+  };
+}
