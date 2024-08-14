0f64ec76290f6c1579493c88419aad7c013eab91
Régis Hanol <regis@hanol.fr>
2024-08-14 12:16:43 +0200

    Update flake.nix

--- flake.nix
+++ flake.nix
@@ -1,12 +1,21 @@
 {
   description = "ZogStriP's NixOS flake.";
 
-  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
 
-  outputs = { self, nixpkgs }: {
+    disko.url = "github:nix-community/disko";
+    disko.inputs.nixpkgs.follows = "nixpkgs";
+  };
+
+  outputs = { self, nixpkgs, ... } @ inputs : {
     nixosConfigurations.framezork = nixpkgs.lib.nixosSystem {
       system = "x86_64-linux";
-      modules = [ ./config.nix ];
+      specialAttrs = inputs;
+      modules = [
+        ./disko.nix
+        ./config.nix
+      ];
     };
   };
 }
