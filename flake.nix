9677ab3b62d70879e607d3d99033768a3064888b
Régis Hanol <regis@hanol.fr>
2024-08-14 12:10:20 +0200

    Update flake.nix

--- flake.nix
+++ flake.nix
@@ -6,7 +6,7 @@
   outputs = { self, nixpkgs }: {
     nixosConfigurations.framezork = nixpkgs.lib.nixosSystem {
       system = "x86_64-linux";
-      boot.loader.systemd-boot.enable = true;
+      modules = [ ./config.nix ];
     };
   };
 }
