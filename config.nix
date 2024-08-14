4da929dd158b91e0a1985c203040751fdd96ba24
Régis Hanol <regis@hanol.fr>
2024-08-14 12:28:07 +0200

    Update config.nix

--- config.nix
+++ config.nix
@@ -1,7 +1,7 @@
 {
-  // Enables the "systemd-boot" boot manager
+  # Enables the "systemd-boot" boot manager
   boot.loader.systemd-boot.enable = true;
 
-  // NixOS version this flake was initially created on
+  # NixOS version this flake was initially created on
   system.stateVersion = "24.05";
 }
