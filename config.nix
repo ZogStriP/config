3f7c63efc86373b8de16e014c806d19c36e765fb
Régis Hanol <regis@hanol.fr>
2024-08-16 10:45:37 +0200

    Update config.nix

--- config.nix
+++ config.nix
@@ -1,27 +1,6 @@
-{ username, stateVersion, ... } : {
-  # Update CPU's microcode
-  hardware.cpu.intel.updateMicrocode = true;
-
+{ stateVersion, ... } : {
   # Use the "systemd-boot" boot loader
   boot.loader.systemd-boot.enable = true;
-
-  # System's timezone
-  time.timeZone = "Europe/Paris";
-
-  users = {
-    # Ensure users can't be changed
-    mutableUsers = false;
-
-    # Disable root password
-    users.root.hashedPassword = "!";
-
-    # zogstrip's user account
-    users.${username} = {
-      isNormalUser = true;
-      hashedPassword = "!";
-      extraGroups = [ "wheel" "networkmanager" ];
-    };
-  };
 
   # NixOS version this flake was initially created on
   system.stateVersion = stateVersion;
