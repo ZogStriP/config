7904df08dc4cf6e2264df4f768817747d12fa3da
Régis Hanol <regis@hanol.fr>
2024-08-16 10:54:14 +0200

    Update config.nix

--- config.nix
+++ config.nix
@@ -1,6 +1,23 @@
-{ stateVersion, ... } : {
+{ username, stateVersion, ... } : {
   # Use the "systemd-boot" boot loader
   boot.loader.systemd-boot.enable = true;
+
+  users = {
+    # Ensure users can't be changed
+    mutableUsers = false;
+
+    # Disable root
+    users.root.hashedPassword = "!";
+
+    users.${username} = {
+      # Just a regular user
+      isNormalUser = true;
+      # No need for a password
+      hashedPassword = "";
+      # Can `sudo` and manage network interfaces (LAN, WAN)
+      extraGroups = [ "wheel" "networkmanager" ];
+    };
+  };
 
   # NixOS version this flake was initially created on
   system.stateVersion = stateVersion;
