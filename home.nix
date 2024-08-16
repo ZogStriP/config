6fdb94fd9b721bc7760e855c91b8e1026058195e
Régis Hanol <regis@hanol.fr>
2024-08-16 11:00:00 +0200

    Update home.nix

--- home.nix
+++ home.nix
@@ -4,6 +4,16 @@
   home-manager.useGlobalPkgs = true;
   home-manager.useUserPackages = true;
 
+  # zogstrip's user account
+  users.${username} = {
+    # Just a regular user
+    isNormalUser = true;
+    # No need for a password
+    hashedPassword = "";
+    # Can `sudo` and manage network interfaces (LAN, WAN)
+    extraGroups = [ "wheel" "networkmanager" ];
+  };
+
   home-manager.users.${username} = {
     home.username = username;
     home.homeDirectory = "/home/${username}";
