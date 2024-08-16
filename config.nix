d1ac54f39292df934674309ec7c6c9b0734386f0
Régis Hanol <regis@hanol.fr>
2024-08-16 11:12:02 +0200

    Update config.nix

--- config.nix
+++ config.nix
@@ -1,6 +1,25 @@
 { hostname, stateVersion, ... } : {
-  # Use the "systemd-boot" boot loader
-  boot.loader.systemd-boot.enable = true;
+  boot = {
+    # Use latest kernel
+    kernelPackages = pkgs.linuxPackages_latest;
+
+    loader = {
+      # Don't display the boot loader (press <space> to show it if needed)
+      timeout = 0;
+
+      systemd-boot = {
+        # Enable "systemd-boot" boot loader
+        enable = true;
+        # Disable editing the boot menu
+        editor = false;
+        # Keep a maximum of 5 generations
+        configurationLimit = 5;
+      };
+
+      # Allows NixOS to modify EFI variables
+      efi.canTouchEfiVariables = true;
+    };
+  };
 
   # Machine's name
   networking.hostName = hostname;
