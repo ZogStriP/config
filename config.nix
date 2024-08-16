5475794b366bf46960fb6d99073d3dc8fdd1112a
Régis Hanol <regis@hanol.fr>
2024-08-16 11:00:26 +0200

    Update config.nix

--- config.nix
+++ config.nix
@@ -1,24 +1,12 @@
-{ username, stateVersion, ... } : {
+{ stateVersion, ... } : {
   # Use the "systemd-boot" boot loader
   boot.loader.systemd-boot.enable = true;
 
-  users = {
-    # Ensure users can't be changed
-    mutableUsers = false;
+  # Ensure users can't be changed
+  users.mutableUsers = false;
 
-    # Disable root by setting an impossible password hash
-    users.root.hashedPassword = "!";
-
-    # zogstrip's user account
-    users.${username} = {
-      # Just a regular user
-      isNormalUser = true;
-      # No need for a password
-      hashedPassword = "";
-      # Can `sudo` and manage network interfaces (LAN, WAN)
-      extraGroups = [ "wheel" "networkmanager" ];
-    };
-  };
+  # Disable root by setting an impossible password hash
+  users.users.root.hashedPassword = "!";
 
   # NixOS version this flake was initially created on
   system.stateVersion = stateVersion;
