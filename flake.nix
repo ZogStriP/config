2ba7b5fe55a2b94af8d3a9aca007f2d520640ad3
Régis Hanol <regis@hanol.fr>
2024-08-21 00:23:44 +0200

    Update flake.nix

--- flake.nix
+++ flake.nix
@@ -7,6 +7,8 @@
 
     home-manager.url = "github:nix-community/home-manager";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
+
+    impermanence.url = "github:nix-community/impermanence";
   };
 
   outputs = { nixpkgs, ... } @ inputs : {
