ec44007dc124e1736dd27121b88cb1b64c7ad5bd
Régis Hanol <regis@hanol.fr>
2024-09-05 10:24:27 +0200

    Update home.nix

--- home.nix
+++ home.nix
@@ -47,6 +47,12 @@
       "..." = "cd ../..";
     };
 
+    # Some session variables
+    home.sessionVariables = {
+      BROWSER = "firefox";
+      GTK_THEME = "dark";
+    };
+
     # Programs that don't need configuration
     home.packages = with pkgs; [
       curl
