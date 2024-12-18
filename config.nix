8a0cd0dafcf87199e7a077a5eca9265474f1b33d
zogstrip <regis@hanol.fr>
2024-12-18 11:42:49 +0100

    disable homebrew's hints

--- config.nix
+++ config.nix
@@ -318,7 +318,10 @@ in {
 
         # Environment variables
         sessionVariables = {
+          # Use 1Password's SSH agent
           SSH_AUTH_SOCK = "${home}/Library/Group Containers/2BUA8C4S2C.com.1password/t/agent.sock";
+          # Disable Homebrew's hints
+          HOMEBREW_NO_ENV_HINTS = 1;
         };
 
         # Add directories to PATH
